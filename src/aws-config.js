c2093c4a1b3fdf1d7557ab1f020a6b37c3e41be4
Zeus2318 <masellones124@gmail.com>
2025-09-27 09:15:11 +0800

    second commit

---
+++ src/aws-config.js
@@ -0,0 +1,8 @@
+const awsConfig = {
+  aws_appsync_graphqlEndpoint: 'YOUR_APPSYNC_ENDPOINT',
+  aws_appsync_region: 'YOUR_REGION',
+  aws_appsync_authenticationType: 'API_KEY',
+  aws_appsync_apiKey: 'YOUR_API_KEY',
+};
+
+export default awsConfig;
